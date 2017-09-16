24a4557a8ddd5922a376f8ff5d39fb89d77d8090
DarylANelson <currygod@hotmail.com>
2017-09-16 17:43:01 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace C_sharp_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number;
+
+            Console.WriteLine("Hello world");
+
+            number = Console.ReadLine();
+            Console.WriteLine("may not display correctly {0}",number);
+                
+        }
+    }
+}
